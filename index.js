a7f47bded10ef2db87518331dc11a11d17eba206
Jared Ticotin <jared_m_t@hotmail.com>
2020-10-10 13:48:44 -0400

    cleaned up some code

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ var HOST = hostname();
 // const body = document.querySelector('.btn');
 // const textarea = document.querySelector('textarea');
 
+/**
+ * scrape function is used to find the translation of parameter msg to another language
+ * for now, only translating to mandarin
+ * @param {string} msg 
+ */
 async function scrape(msg="hello"){
     var URL = 'https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-CN&text=';
     let browser = await puppeteer.launch({'headless':true});
     let page = await browser.newPage();
-    //URL+=textarea.innerText;
-    console.log(msg);
-    URL+=encodeURIComponent(msg);
-    console.log(URL);
+    URL+=encodeURIComponent(msg);//add message to the URL to be scraped
 
     await page.goto(URL,{waitUntil:'networkidle2'});
     /*networkidle2 waits until there are no more than 2 network connections for
     at least half a second*/
 
-    //translate to chinese characters
+    //translate to chinese characters and pinyin
     let result = await page.evaluate(()=>{
         return {mandarin: document.querySelector('.result-shield-container span').innerHTML,
                 pinyin: document.querySelector('.tlid-result-transliteration-container .tlid-transliteration-content').innerText
@@ -33,22 +35,27 @@ async function scrape(msg="hello"){
         };
     });
 
-    //get the pinion
-    await browser.close();
+    await browser.close();//close headless browser
 
-    //console.log("mandarin: ",result.mandarin,", pinyin: ",result.pinyin);
+    //add the original messsage to result and return the result.
     result['msg']=msg;
     return result;
 
 };
 
 
+/**
+ * create the server
+ */
 let server = http.createServer(async (req,resp)=>{
-    var URL = req.url.split('?')[0];
+    var URL = req.url.split('?')[0];//get url not including query
+
+    //get the input value from the URL
     let inputValue=url.parse(req.url,true).query;
     inputValueGiven=Object.keys(inputValue).indexOf('input')>=0;
     inputValue = inputValueGiven?inputValue['input']:"";
     
+    //get the complete path and the file extension
     let filepath = path.join(__dirname,URL=='/'?'index.html':URL);
     let extname = path.extname(filepath);
 
@@ -78,35 +85,33 @@ let server = http.createServer(async (req,resp)=>{
         return;//ignore icons for now
     }
     
+    //update header
     await resp.writeHead(200,{'Content-Type':contentType});
+
+    //scrape google translate to get the results if input value given
     var result="";
     if (inputValueGiven){
         result = await scrape(inputValue);//message goes here!
     }
+
+    //save results to json file (will be blank if no input value was given)
     await fs.writeFile('./translate.json',JSON.stringify(result),err=>console.log(err));
-    //await resp.write([result.msg,result.pinyin,result.mandarin].join(", "));
+
+    //respond with the file requested by client
     await fs.readFile(filepath,'utf8',(err,data)=>{
         if(err){
-            console.log(err);
+            console.log("error: ",err);
             return "error";
         }
         //return data;
         resp.write(data,'utf8');
         resp.end();
     });
-    //console.log("index: ",await index, path.join(__dirname,'index.html'));
-    // await resp.write(index,'utf8',err=>console.log(err));
-    // //console.log(await result.mandarin);
-    // await resp.end();
     
     
 });
 
 
 
-// //usage of scrape (testing)
-// let runscrape = async ()=>{
-//     console.log(await scrape());
-// }
 
 server.listen(PORT);
